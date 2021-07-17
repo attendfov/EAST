8943737714219c41e5b0175c7ff6bf7684406fbd
junhuang.hj <Attendfov@163.com>
2021-07-17 08:07:58 +0800

    修改代码结构

---
+++ utils/config.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+
+#common
+tf.app.flags.DEFINE_string('gpu_list', '1', '')
+tf.app.flags.DEFINE_string('checkpoint_path', '/tmp/east_resnet_v1_50_rbox/', '')
+tf.app.flags.DEFINE_integer('input_size', 512, '')
+
+#multi-gpu train
+tf.app.flags.DEFINE_integer('batch_size_per_gpu', 14, '')
+tf.app.flags.DEFINE_integer('num_readers', 16, '')
+tf.app.flags.DEFINE_float('learning_rate', 0.0001, '')
+tf.app.flags.DEFINE_integer('max_steps', 100000, '')
+tf.app.flags.DEFINE_float('moving_average_decay', 0.997, '')
+tf.app.flags.DEFINE_boolean('restore', False, 'whether to resotre from checkpoint')
+tf.app.flags.DEFINE_integer('save_checkpoint_steps', 1000, '')
+tf.app.flags.DEFINE_integer('save_summary_steps', 100, '')
+tf.app.flags.DEFINE_string('pretrained_model_path', None, '')
+
+#data layer
+tf.app.flags.DEFINE_string('training_data_path', '/data/ocr/icdar2015/',
+                           'training dataset to use')
+tf.app.flags.DEFINE_string('training_data_path', '/data/ocr/icdar2015/',
+                           'training dataset to use')
+
+tf.app.flags.DEFINE_integer('max_image_large_side', 1280,
+                            'max image size of training')
+tf.app.flags.DEFINE_integer('max_text_size', 800,
+                            'if the text in the input image is bigger than this, then we resize'
+                            'the image according to this')
+tf.app.flags.DEFINE_integer('min_text_size', 10,
+                            'if the text size is smaller than this, we ignore it during training')
+tf.app.flags.DEFINE_float('min_crop_side_ratio', 0.1,
+                          'when doing random crop from input image, the'
+                          'min length of min(H, W')
+tf.app.flags.DEFINE_string('geometry', 'RBOX',
+                           'which geometry to generate, RBOX or QUAD')
+
+
+
+#infer
+tf.app.flags.DEFINE_string('test_data_dir', '/data/ocr/icdar2015/images')
+tf.app.flags.DEFINE_string('output_dir', '/data/ocr/icdar2015/inference')
+
+tf.app.flags.DEFINE_string('is_write_images', True, 'if save images or not')
+tf.app.flags.DEFINE_string('max_side_length', 512, 'max side length')
+tf.app.flags.DEFINE_string('save_score_map', False, 'if save score map or not')
+tf.app.flags.DEFINE_string('enable_box_refinement', False, 'refine boxes')
+tf.app.flags.DEFINE_string('save_inference_graph', True, 'if save inference graph or not')
+
+
+FLAGS = tf.app.flags.FLAGS
